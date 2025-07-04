a6751bf353bd23cce1aed2213b05613385e117b9
kirubhel <kirub.hel@gmail.com>
2025-07-04 11:48:27 +0300

    final-task-3

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -53,8 +53,7 @@ Answer:"""
 from transformers import pipeline
 
 # Load local model pipeline
-qa_pipeline = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.2", max_new_tokens=200)
-
+qa_pipeline = pipeline("text2text-generation", model="google/flan-t5-base", max_new_tokens=200)
 def generate_answer(prompt):
     return qa_pipeline(prompt)[0]["generated_text"]
 
